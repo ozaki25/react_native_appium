a0185037402f71c55ee30d94ac3c878bc83adae8
ozaki <fs65ugumori@yahoo.co.jp>
2018-08-14 23:23:00 +0900

    update test case

--- __tests__/sample.test.js
+++ __tests__/sample.test.js
@@ -11,26 +11,50 @@ const config = {
 };
 const driver = wd.promiseChainRemote('localhost', PORT);
 
+const setup = async () => {
+  await driver.init(config);
+  await driver.sleep(4000); // wait for app to load
+};
+
 describe('e2e', () => {
-  beforeAll(async () => {
-    await driver.init(config);
-    await driver.sleep(5000); // wait for app to load
+  describe('page navigation', () => {
+    beforeAll(setup);
+    it('show screen1', async () => {
+      expect(await driver.hasElementByAccessibilityId('screen1')).toBe(true);
+    });
+    it('show screen2', async () => {
+      await driver.elementByAccessibilityId('sample1').click();
+      expect(await driver.hasElementByAccessibilityId('screen2')).toBe(true);
+    });
+    it('show screen3', async () => {
+      await driver.elementByAccessibilityId('sample2').click();
+      expect(await driver.hasElementByAccessibilityId('screen3')).toBe(true);
+    });
+    it('show screen1 again', async () => {
+      await driver.elementByAccessibilityId('sample3').click();
+      expect(await driver.hasElementByAccessibilityId('screen1')).toBe(true);
+    });
   });
 
-  it('page navigation test', async () => {
-    expect(await driver.hasElementByAccessibilityId('screen1')).toBe(true);
-    await driver.elementByAccessibilityId('sample1').click();
-    expect(await driver.hasElementByAccessibilityId('screen2')).toBe(true);
-    await driver.elementByAccessibilityId('sample2').click();
-    expect(await driver.hasElementByAccessibilityId('screen3')).toBe(true);
-    await driver.elementByAccessibilityId('sample3').click();
-    expect(await driver.hasElementByAccessibilityId('screen1')).toBe(true);
-  });
-
-  it('input text test', async () => {
-    const name = 'オザキ';
-    await driver.elementByAccessibilityId('inputName').type(name);
-    const result = await driver.elementByAccessibilityId('name').text();
-    expect(result).toBe(name);
+  describe('input name', () => {
+    beforeEach(setup);
+    it('input alphabet', async () => {
+      const name = 'ozaki';
+      await driver.elementByAccessibilityId('inputName').type(name);
+      const result = await driver.elementByAccessibilityId('name').text();
+      expect(result).toBe(name);
+    });
+    it('input kana', async () => {
+      const name = 'オザキ';
+      await driver.elementByAccessibilityId('inputName').type(name);
+      const result = await driver.elementByAccessibilityId('name').text();
+      expect(result).toBe(name);
+    });
+    it('input kanji', async () => {
+      const name = '尾崎';
+      await driver.elementByAccessibilityId('inputName').type(name);
+      const result = await driver.elementByAccessibilityId('name').text();
+      expect(result).toBe(name);
+    });
   });
 });
