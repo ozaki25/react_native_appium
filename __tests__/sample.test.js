81a114519af4b8e1719c7170ab9f037c1e3b5e6e
ozaki <fs65ugumori@yahoo.co.jp>
2018-08-14 22:11:21 +0900

    add text input test

--- __tests__/sample.test.js
+++ __tests__/sample.test.js
@@ -9,17 +9,26 @@ const config = {
 };
 const driver = wd.promiseChainRemote('localhost', PORT);
 
-beforeAll(async () => {
-  await driver.init(config);
-  await driver.sleep(2000); // wait for app to load
-});
+describe('e2e', () => {
+  beforeAll(async () => {
+    await driver.init(config);
+    await driver.sleep(2000); // wait for app to load
+  });
 
-test('page navigation test', async () => {
-  expect(await driver.hasElementByAccessibilityId('screen1')).toBe(true);
-  await driver.elementByAccessibilityId('sample1').click();
-  expect(await driver.hasElementByAccessibilityId('screen2')).toBe(true);
-  await driver.elementByAccessibilityId('sample2').click();
-  expect(await driver.hasElementByAccessibilityId('screen3')).toBe(true);
-  await driver.elementByAccessibilityId('sample3').click();
-  expect(await driver.hasElementByAccessibilityId('screen1')).toBe(true);
+  it('page navigation test', async () => {
+    expect(await driver.hasElementByAccessibilityId('screen1')).toBe(true);
+    await driver.elementByAccessibilityId('sample1').click();
+    expect(await driver.hasElementByAccessibilityId('screen2')).toBe(true);
+    await driver.elementByAccessibilityId('sample2').click();
+    expect(await driver.hasElementByAccessibilityId('screen3')).toBe(true);
+    await driver.elementByAccessibilityId('sample3').click();
+    expect(await driver.hasElementByAccessibilityId('screen1')).toBe(true);
+  });
+
+  it('input text test', async () => {
+    const name = 'OZAKI';
+    await driver.elementByAccessibilityId('inputName').type(name);
+    const result = await driver.elementByAccessibilityId('name').text();
+    expect(result).toBe(name);
+  });
 });
--- src/components/Sample1.js
+++ src/components/Sample1.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -10,15 +10,31 @@ const styles = StyleSheet.create({
   },
 });
 
-const Sample1 = ({ navigation }) => (
-  <TouchableOpacity
-    style={styles.container}
-    onPress={() => navigation.navigate('Sample2')}
-    accessibilityLabel="screen1"
-  >
-    <Text accessibilityLabel="sample1">Sample1</Text>
-  </TouchableOpacity>
-);
+class Sample1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '' };
+  }
+
+  render() {
+    const { navigation } = this.props;
+    return (
+      <TouchableOpacity
+        style={styles.container}
+        onPress={() => navigation.navigate('Sample2')}
+        accessibilityLabel="screen1"
+      >
+        <Text accessibilityLabel="sample1">Sample1</Text>
+        <TextInput
+          onChangeText={name => this.setState({ name })}
+          value={this.state.name}
+          accessibilityLabel="inputName"
+        />
+        <Text accessibilityLabel="name">{this.state.name}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
 
 Sample1.navigationOptions = { title: 'Sample1' };
 
